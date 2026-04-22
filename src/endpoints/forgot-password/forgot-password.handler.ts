7af8f3ef3cfbfc8e07eb33365fde1e20f1375b0d
shanmugavel-gwc <shanmugavel.muthuraj@gwcdata.ai>
2026-04-22 14:12:55 +0530

    Update frontend URL for password reset email
    Signed-off-by: shanmugavel-gwc <shanmugavel.muthuraj@gwcdata.ai>

--- src/endpoints/forgot-password/forgot-password.handler.ts
+++ src/endpoints/forgot-password/forgot-password.handler.ts
@@ -58,7 +58,7 @@ export const forgotPasswordHandler: EndpointHandler<EndpointAuthType.NONE> = asy
     reportInfo(`Reset token stored in DB. Attempting to send email to ${user.email}...`);
 
     // Send Email
-    const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:5173';
+    const frontendUrl = process.env.FRONTEND_URL || 'http://34.100.224.44';
     const resetLink = `${frontendUrl}/reset-password/${resetToken}`;
 
     const mailOptions = {
